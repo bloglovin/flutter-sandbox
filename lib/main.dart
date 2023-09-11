13eb884b0b4bc0d884e657b1e2edb9fca55a77d8
Tristen Paul <tristenp9711@gmail.com>
2023-09-11 13:20:15 +0200

    Added a reset button just for fun

--- lib/main.dart
+++ lib/main.dart
@@ -55,16 +55,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int counter = 0;
+  bool isResetDisabled = true;
 
-  void _incrementCounter() {
+  void incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      counter++;
+      isResetDisabled = false;
+    });
+  }
+
+  void resetCounter() {
+    setState(() {
+      counter = 0;
+      isResetDisabled = true;
     });
   }
 
@@ -108,15 +117,21 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+              child: Text(
+                '$counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ),
+            ElevatedButton(
+                onPressed: isResetDisabled ? null: resetCounter,
+                child: const Text("Reset"))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
